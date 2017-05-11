90ac68d90514ef92240cf2ae86846c8009af6300
ericgoalist <e.nagata@goalist.co.jp>
2017-05-11 16:18:00 +0900

    復習１

---
+++ src/training5_11/Training5_11.java
@@ -0,0 +1,13 @@
+package training5_11;
+
+import java.util.Calendar;
+
+public class Training5_11 {
+	public static void main(String[] args) {
+		Calendar calendar = Calendar.getInstance();
+		int month = calendar.get(Calendar.MONTH) + 1;
+		int date = calendar.get(Calendar.DAY_OF_MONTH);
+		String message = "今日の日付は" + month + "月" + date + "日です";
+		System.out.println(message);
+	}
+}
